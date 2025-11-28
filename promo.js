1c1f40ff65a0acfe0f23ae5ad69097e465df556d
TimofeevMaksim <98007028+TimofeevMaksim@users.noreply.github.com>
2025-11-28 23:36:33 +0300

    Delete promo.js

--- promo.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function() {
-    setBodyLoaded();
-});
-
-function setBodyLoaded() {
-    var body = document.getElementsByTagName('body')[0];
-    body.classList.add('loaded');
-}
